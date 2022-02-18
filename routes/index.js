b52c40985044ef783d62c23700536c931e7baba7
Chuanjo <chuanjo@hotmail.com>
2022-02-18 17:33:24 +0100

    Iteracion 1 lista

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
