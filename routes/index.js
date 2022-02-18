4fe81c720c2518fc2db1774ae2ec9d872f54e8ab
Chuanjo <chuanjo@hotmail.com>
2022-02-18 20:12:15 +0100

    Completado sin CSS

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,35 @@ const express = require('express');
 const router = express.Router();
 const Movie = require("../models/movie.model")
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+/* Home page */
+
+router.get('/', (req, res, next) => res.render('index'))
+
+/* Movies page */
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then((response)=>{
+        console.log(response)
+        res.render("movies.hbs", {response})
+    })
+    .catch((err)=>{
+        next(err)
+    })
+})
+
+/* Movies detailed page */
+
+router.get("/movies/:id",(req,res,next)=>{
+
+    Movie.findById(req.params.id)
+    .then((response) =>{
+      res.render("movie-details.hbs", {response})
+    })
+    .catch((err)=>{
+      console.log(err)
+      next(err)
+    })
+  })
 
 module.exports = router;
